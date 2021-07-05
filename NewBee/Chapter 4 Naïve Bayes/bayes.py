6dc2700860800e21e655a2bce887e49eaa3eec14
GuanBam <37057534+GuanBam@users.noreply.github.com>
2021-07-05 18:12:34 -0500

    Update bayes.py

--- NewBee/Chapter 4 Naïve Bayes/bayes.py
+++ NewBee/Chapter 4 Naïve Bayes/bayes.py
@@ -1,10 +1,17 @@
 '''
 Created on Oct 19, 2010
-
+Python -V: 2
 @author: Peter
+
+Modified on July 5, 2021
+Python -V:3.7.4
+@author: GuanBam
 '''
 from numpy import *
-
+"""
+Generate example dataset
+Output: Sentence Matrixs, label array
+"""
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -14,13 +21,22 @@ def loadDataSet():
                  ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
     classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
     return postingList,classVec
-                 
+"""
+Create unique list based on given dataset
+Input: Sentence Matrixs
+Output: Unique set based on input
+"""
 def createVocabList(dataSet):
     vocabSet = set([])  #create empty set
     for document in dataSet:
         vocabSet = vocabSet | set(document) #union of the two sets
     return list(vocabSet)
 
+"""
+Record if the word in input set occured in vocablist
+Input: Unqiue word list, Sentence matrixs
+Output: Array to record the Words occurred in Unique Word List
+"""
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
@@ -29,6 +45,11 @@ def setOfWords2Vec(vocabList, inputSet):
         else: print( "the word: %s is not in my Vocabulary!" % word)
     return returnVec
 
+"""
+Calculate Probability according Naïve Bayes Algorithm
+Input: Sentence Matrix, Sentence Label
+Output: Probability P(w|c0), Probability P(w|c1), Probability P(c1)
+"""
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
@@ -45,9 +66,13 @@ def trainNB0(trainMatrix,trainCategory):
     p1Vect = log(p1Num/p1Denom)          #change to log()
     p0Vect = log(p0Num/p0Denom)          #change to log()
     return p0Vect,p1Vect,pAbusive
-
+"""
+Match the label of input according to the probability
+Input: Unique Word List, P(w|c0), P(w|c1), p(c1)
+Output: Label
+"""
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
-    p1 = sum(vec2Classify * p1Vec) + log(pClass1)    #element-wise mult
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
     p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
     if p1 > p0:
         return 1
